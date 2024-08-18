4b85b7b2a6e5a3c543253734238e9444169adcdc
Bhargav Das <bhargavdas77@hotmail.com>
2024-08-18 20:45:44 +0530

    day_49_LinkedIN bot start

---
+++ LinkedIN_bot/main.py
@@ -0,0 +1,34 @@
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.common import keys
+from dotenv import load_dotenv
+from selenium.webdriver.common.by import By
+
+load_dotenv()
+LINKED_IN = "https://www.linkedin.com/checkpoint/lg/sign-in-another-account"
+ID = os.environ["LOGIN_ID"]
+PASS = os.environ["PASSWRD"]
+JOB_LINK = os.environ["JOB_LINK"]
+
+
+driver = webdriver.Firefox()
+keys = keys.Keys()
+driver.get(url=LINKED_IN)
+
+username = driver.find_element(By.NAME, value="session_key")
+password = driver.find_element(By.NAME, value="session_password")
+button = driver.find_element(By.XPATH, value="/html/body/div/main/div[2]/div[1]/form/div[3]/button")
+
+username.send_keys(ID)
+password.send_keys(PASS)
+time.sleep(10)
+button.click()
+
+
+
+
+
+
+
+
